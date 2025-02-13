040c684419c388945f4f1b453e40cfd657813276
kasun aththanagoda <kasunaththanagoda@gmail.com>
2025-02-13 22:53:30 +0530

    second commit

---
+++ src/main/java/com/ecommerce/first_ecomm/FirstEcommApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.first_ecomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstEcommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstEcommApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/first_ecomm/FirstEcommApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.first_ecomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstEcommApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
